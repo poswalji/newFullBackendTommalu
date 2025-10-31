bf4aadb6ffeaafce915c7795be12056efa69f5c8
ashutosh <mr.luckysharma7@gmail.com>
2025-10-31 12:52:36 +0530

    .

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const allowedOrigins = [
   "http://localhost:3000",       // React dev server
   "http://localhost:5173",       // Vite dev server
   "http://127.0.0.1:3000",      // Localhost variants
-  "http://127.0.0.1:5173"
+  "http://127.0.0.1:5173",
+  "https://tommalubackendservice.vercel.app"
 ];
 
 app.use(cors({
