70ea8d3682b9b78de9a22be1ca8315e2db8959de
ashutosh <mr.luckysharma7@gmail.com>
2025-11-06 18:40:07 +0530

    .

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ process.on('uncaughtException', (err) => {
 });
 
 const allowedOrigins = [
-  "https://tommalu.netlify.app",
+  process.env.FRONTEND_URL,
   "http://localhost:3000",
   "http://localhost:3001",
   "http://localhost:5173",
